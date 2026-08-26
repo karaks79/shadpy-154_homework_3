712371bcceeb248676a4091e93293f626d8632ec
karaks79 <antinarit@gmail.com>
2026-08-26 20:01:04 +0300

    correct

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import bs4 
 from fake_headers import Headers
 from datetime import datetime
+from urllib.parse import urljoin
 
 
 ## Определяем список ключевых слов:
@@ -28,13 +29,21 @@ def main():
         #print(article_date_str)
 
         article_tag = soup_article.find('h2', class_='tm-title tm-title_h2')
-
         article_title = article_tag.text.strip()
         #print(article_title)
 
-        article_link = 'https://habr.com' + article_tag.find('a')['href']
+        #article_link = 'https://habr.com' + article_tag.find('a')['href']
+        article_link = urljoin('https://habr.com', article_tag.find('a')['href'])
+        if not article_link.startswith('https://habr.com/'):
+            continue
         #print(article_link)
 
+        article_prewiew_tag = soup_article.find('div', class_='lead')
+        article_prewiew_text = article_prewiew_tag.text.strip()
+        if any(word.lower() in article_prewiew_text.lower() for word in KEYWORDS):
+            print(f"{article_date_str} - {article_title} - {article_link}")
+
+        #--Дополнительное (необязательное) задание -----------------------------
         response_full_article = requests.get(article_link, headers=headers)
         soup_full_article = bs4.BeautifulSoup(response_full_article.text, features='lxml')
         full_article_text = soup_full_article.find('div', class_='tm-article-presenter__body').text.strip()
